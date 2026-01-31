b14df689d67829afd12f0353edc59a0455f5d973
FRIGL <M4s0nm@outlook.com>
2026-01-31 17:06:48 -0500

    Worked on player controller

--- ggj-2026/Scripts/player/player_movement_controller.gd
+++ ggj-2026/Scripts/player/player_movement_controller.gd
@@ -3,8 +3,10 @@ extends RigidBody2D
 
 ##got the RigidBody based movemnt code form here: https://www.youtube.com/watch?v=PKPcoR_yIvE
 @export var gravity = 980
+
 @export var groundMovementForce = 1000 # How fast the player will move (pixels/sec).
-@export var groundDragForce = 1000; 
+@export var groundDragForce = 1000; # How fast the player will stop (pixels/sec).
+
 @export var sidewaysAirMovementForce = 1000
 @export var downwardAirMovementForce = 1000
 @export var jumpStrength = 500
@@ -15,13 +17,7 @@ extends RigidBody2D
 
 var inputVector:Vector2
 var grounded = false
-var direction = Vector2.ZERO # The player's movement direction vector.
 var timeSinceLastTouchedGround = 0;
-var canJump = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,96 +33,53 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		inputVector.y -= 1
 	inputVector = inputVector.normalized()
-	
-	print("inputVector:" + str(inputVector));
-	
 
 
-
-
-
-
-#update loop
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	
-	#check if the player is touching the gound
+	##GroundCheck############################
 	grounded = ((right_ray_cast.is_colliding() == true) || (right_ray_cast.is_colliding() == true))
 	
-	##GravityCode############################
-
-	##customizable gravity 
+	##Gravity############################
 	state.apply_central_force(Vector2(0, gravity))
-	
-	##MovemntCode############################
-	
 	
 
 	if(grounded == true):
-		state.apply_central_force(Vector2(inputVector.x * groundMovementForce, 0));
-	elif(grounded == false):
-		state.apply_central_force(inputVector * sidewaysAirMovementForce);
+		##GroundMovement############################
+		var inputVelocityDelta:float = inputVector.x * groundMovementForce * state.step
+		var frictionVelocityDelta:float = move_toward(0, -self.linear_velocity.x, groundDragForce * state.step)
+
+		if(sign(inputVelocityDelta) == sign(frictionVelocityDelta)):
+			#if the player is applying input, and the friction would be in the same direction, only apply the friction
+			self.linear_velocity.x += frictionVelocityDelta
+		elif(inputVector.x == 0):
+			#if the player is not applying any input, only add frictionVelocityDelta
+			self.linear_velocity.x += frictionVelocityDelta
+		else:
+			#if the player is applying input, and the friction would fight agisnt the players input, only apply the players input
+			self.linear_velocity.x += inputVelocityDelta
+
 	
-	##ground-friction Logic
-	## if the player is not trying to move left of right and is on the ground, apply a force to brin them to a stop
-	if((grounded == true) && (inputVector == Vector2.ZERO)):
-		var playerVelocity = self.linear_velocity;
-		playerVelocity.x = move_toward(playerVelocity.x, 0, groundDragForce * state.step)
-		self.linear_velocity = playerVelocity;
+	elif(grounded == false):
+		##AirMovement############################
+		#downward plunge movement
+		if(inputVector.y > 0):
+			state.apply_central_force(Vector2.DOWN * inputVector.y * downwardAirMovementForce);
+			
+		#in air left/right movement
+		state.apply_central_force(Vector2(inputVector.x * sidewaysAirMovementForce, 0));
 
-	##jumpCode#################################
-
-	##if we are grounded we can jump
+	##JumpCode#################################
+	#if we are grounded we can jump
 	if(grounded == true):
 		timeSinceLastTouchedGround = coyotetime;
 	
-	##if we are still in coyotetime, count down untill we are not 
+	#if we are still in coyotetime, count down until we are not 
 	if(timeSinceLastTouchedGround != 0):
 		timeSinceLastTouchedGround = move_toward(timeSinceLastTouchedGround, 0, state.step)
 
-	##if we are in coyotetime, we can jump 
+	#if we are in coyotetime, we can jump 
 	if(timeSinceLastTouchedGround != 0):
 		if Input.is_action_pressed("move_up"):
 			timeSinceLastTouchedGround = 0;##we can no longer jump
 			set_linear_velocity( Vector2(get_linear_velocity().x, -jumpStrength) );
-	
-	###########################################
-
-	#animation logic 
-	#if direction.length() > 0:
-		###$AnimatedSprite.play()
-		#get_node("AnimatedSprite").play()
-	#else:
-		###$AnimatedSprite.stop()
-		#get_node("AnimatedSprite").stop()
-		
-	#if(grounded == true):	
-		#if direction.x != 0:
-			#$AnimatedSprite.animation = "run"
-			###$AnimatedSprite.flip_v = false
-			#$AnimatedSprite.flip_h = direction.x < 0
-		#else:
-			#$AnimatedSprite.animation = "idle"
-	#else: 
-		#if self.linear_velocity.y < 0:
-			#$AnimatedSprite.animation = "jump"
-		#else:
-			#$AnimatedSprite.animation = "fall"
-#
-		#$AnimatedSprite.flip_h = self.linear_velocity.x < 0
-
-
-	##elif direction.y != 0:
-	##	$AnimatedSprite.animation = "up"
-	##	$AnimatedSprite.flip_v = direction.y > 0
-	##else:
-	##	$AnimatedSprite.animation = "idle"
-		
-
-func _on_CanJump_Tigger_Zone_Area2D_body_entered(body):
-	if(body.name != "Player"):
-		grounded = true
-
-func _on_CanJump_Tigger_Zone_Area2D_body_exited(body):
-	if(body.name != "Player"):
-		grounded = false
