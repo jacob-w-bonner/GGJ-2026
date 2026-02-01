66b9e952f2d5de9934cc516abc45b7b7330dd6aa
Allie <93688459+a-llie@users.noreply.github.com>
2026-02-01 13:23:01 -0500

    fix game over

--- ggj-2026/Scripts/fadeToBlack.gd
+++ ggj-2026/Scripts/fadeToBlack.gd
@@ -1,6 +1,7 @@
 extends CanvasModulate
 
-@onready var game_over_ui = $Game_over_restart
+@onready var game_over_ui = $"../Game_over_restart"
+@onready var restart_level_button = $"../RestartButton"
 
 signal fade_done 
 
@@ -11,7 +12,6 @@ var game_overing = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_over_ui.visible = false
-	game_overing = true
 	pass # Replace with function body.
 
 
@@ -19,23 +19,26 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if fading_out: 
 		fade(delta, 0)
-		if (color.r <= 0): 
-			fade_done.emit()
-	if game_overing: 
-		fade(delta, 75)
-		if (color.r <= 75):
+		if (color.r <= 0.01):
+			print("reloading!") 
+			get_tree().reload_current_scene()
+	elif game_overing: 
+		fade(delta, .5)
+		if (color.r <= .75):
 			game_over_ui.visible = true
 			
 
 func _on_game_over():
+	fading_out = false
 	game_overing = true	
-
+	restart_level_button.visible = false 
+	
 func _on_fade_out():
 	fading_out = true
+	game_overing = false
 
 
 func fade(delta, stop_point): 
-	
 	var rgbVal = color.r - delta*speed
 	if (rgbVal > stop_point):
 		color = Color(rgbVal, rgbVal, rgbVal, 255)
--- ggj-2026/Scripts/game_over_restart.gd
+++ ggj-2026/Scripts/game_over_restart.gd
@@ -1,16 +1,4 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("scene started")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _pressed():
 	get_tree().reload_current_scene() 
