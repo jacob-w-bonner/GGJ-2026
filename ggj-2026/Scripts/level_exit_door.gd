3b7b8f90185058afd14fd456cd6baa8c020fcfa2
Allie <93688459+a-llie@users.noreply.github.com>
2026-02-01 16:49:29 -0500

    ui, exit door

--- ggj-2026/Scripts/level_exit_door.gd
+++ ggj-2026/Scripts/level_exit_door.gd
@@ -4,9 +4,38 @@ extends Area2D
 @export var player_root:PlayerRoot 
 
 
+@export var puzzle1: Node2D
+@export var puzzle2: Node2D
+@export var puzzle3: Node2D
+@export var puzzle4: Node2D
+@export var puzzle5: Node2D
+
+@onready var key1 = $Sprite2D/Puzzle1
+@onready var key2 = $Sprite2D/Puzzle2
+@onready var key3 = $Sprite2D/Puzzle3
+@onready var key4 = $Sprite2D/Puzzle4
+@onready var key5 = $Sprite2D/Puzzle5
+
+var puzzles = 0
+
+var completedPuzzles = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	if (puzzle1 == null):
+		key1.visible = false
+		puzzles += 1
+	if (puzzle2 == null):
+		key2.visible = false
+		puzzles +=1
+	if (puzzle3 == null):
+		key3.visible = false
+		puzzles +=1 
+	if (puzzle4 == null):
+		key4.visible = false
+		puzzles +=1
+	if (puzzle5 == null):
+		key5.visible = false 
+		puzzles +=1
 	
 	
 	pass # Replace with function body.
@@ -19,7 +48,8 @@ func _process(delta: float) -> void:
 	#print(get_tree().current_scene.name)
 	if(player_root != null):
 		var distanceToPlayer:float = player_root.get_controller_pos().distance_to(self.position)
-		if(distanceToPlayer < 80):
+		
+		if(distanceToPlayer < 80 && open_door()):
 			go_to_next_level()
 
 #TODO: Change scenes accordingly
@@ -33,4 +63,23 @@ func go_to_next_level() -> void:
 
 	if get_tree().current_scene.name == "Level 3": 
 		get_tree().change_scene_to_file("res://Scenes/You Win.tscn")
+		
+
+func open_door(): 
+	if (puzzles <= completedPuzzles):
+		return true
+	return false
+
+func _on_completed_puzzle():
+	completedPuzzles+=1
+	if (completedPuzzles == 1):
+		key1.play("solved")
+	elif (completedPuzzles == 2):
+		key2.play("solved")
+	elif (completedPuzzles == 3):
+		key3.play("solved")
+	elif (completedPuzzles == 4):
+		key4.play("solved")
+	elif (completedPuzzles == 5):
+		key5.play("solved")
 		
