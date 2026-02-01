09cbc5741aa3638557fe33122a2b5d49beeb6688
Paige N <paige@rosenquist.com>
2026-02-01 12:54:07 -0500

    Added door asset

--- ggj-2026/Scripts/level_exit_door.gd
+++ ggj-2026/Scripts/level_exit_door.gd
@@ -3,9 +3,6 @@ extends Area2D
 @export var test:float = 0
 @export var player:Node2D 
 
-#TODO: Delete 
-var counter:int = 0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,16 +17,15 @@ func _process(delta: float) -> void:
 		if(distanceToPlayer < 80):
 			go_to_next_level()
 
+#TODO: Change scenes accordingly
 func go_to_next_level() -> void:
-	print("---debug" + str(counter))
-	counter += 1
 	# Based on current scene, move to the next scene
 	if get_tree().current_scene.name == "Level 1":
-		print("Go to Level 2! " + str(counter))
+		print("Go to Level 2!")
 		
 	if get_tree().current_scene.name == "Level 2":
-		print("Go to Level 3! " + str(counter))
+		print("Go to Level 3!")
 		
 	if get_tree().current_scene.name == "Level 3": 
-		print("You  beat the game!" + str(counter))
+		print("You  beat the game!")
 		
