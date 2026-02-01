e2229555e8fe0fb9b511d2b01b22c4f0d6396d34
FRIGL <M4s0nm@outlook.com>
2026-01-31 23:01:41 -0500

    Added a absorbable_colour::TrueIfArgumentColourIsWithinTolerance() function

--- ggj-2026/Scripts/absorbable_colour.gd
+++ ggj-2026/Scripts/absorbable_colour.gd
@@ -3,6 +3,28 @@ class_name AbsorbableColor
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+#testing the TrueIfArgumentColourIsWithinTolerance() function
+#func _ready() -> void:
+	#var c1:Color = Color.from_hsv(0.66,1,1,1)
+	#var a:bool = TrueIfArgumentColourIsWithinTolerance(c1)
+	#print("a:" + str(a))
 
 func GetColor() -> Color:
 	return sprite_2d.self_modulate
+
+
+#returns true if the argumentColour is close to this AbsorbableColor's colouro
+func TrueIfArgumentColourIsWithinTolerance(argumentColour:Color) -> bool:
+	#Controls how far off the argumentColour's hue can be for this function to return true
+	var hueDifferenceTolerance = 0.3333#
+	
+	var ourHue:float = sprite_2d.self_modulate.h
+	var argumentHue:float = argumentColour.h
+	
+	#We need to do all this shit because the hues are circular, but like numbers arent.... you know what I mean?
+	var hueDifference_upShifted = (argumentHue + 1) - ourHue
+	var hueDifference_normal = ourHue - argumentHue
+	var hueDifference_downShifted = (argumentHue - 1) - ourHue
+	var realHueDifference:float = min(min(abs(hueDifference_upShifted), abs(hueDifference_normal)), abs(hueDifference_downShifted))
+	
+	return (realHueDifference <= hueDifferenceTolerance)
