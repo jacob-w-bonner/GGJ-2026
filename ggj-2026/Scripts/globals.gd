b0c655cc0a416d13049159fb40a18a6bd764f530
BigBossJacob <jacob.bonner@rogers.com>
2026-02-01 11:53:10 -0500

    Added Gradients to Absorb and Equalize

--- ggj-2026/Scripts/globals.gd
+++ ggj-2026/Scripts/globals.gd
@@ -5,7 +5,7 @@ const PLAYER_X: 	float  	= 16.0
 const PLAYER_Y: 	float 	= 16.0
 
 # Scale for the player
-const PLAYER_SCALE: Vector2 = Vector2(2, 2)
+const PLAYER_SCALE: Vector2 = Vector2(3.5, 3.5)
 
 # Default colour
 var DEFAULT_COL: 	Color 	= Color(1.0, 1.0, 1.0, 1.0)
