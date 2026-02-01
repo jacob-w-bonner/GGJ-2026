c5bbe41755ae709c5de324cdb8c351d7e59d8b5f
BigBossJacob <jacob.bonner@rogers.com>
2026-01-31 21:33:45 -0500

    Added Basic Colour Absorption Functionality

--- ggj-2026/Scripts/absorbable_colour.gd
+++ ggj-2026/Scripts/absorbable_colour.gd
@@ -1,4 +1,5 @@
 extends Area2D
+class_name AbsorbableColor
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
--- ggj-2026/Scripts/colour_cells.gd
+++ ggj-2026/Scripts/colour_cells.gd
@@ -15,26 +15,19 @@ func _process(_delta: float) -> void:
 
 # This function creates colour cells
 func create_colour_cells() -> void:
-
-	# Scaling for the cells
-	var scale_set = Vector2(1, 1)
 	
 	# Creating a series of ColorRects that are the colour cells of the sponge
 	for y in range(int(Globals.PLAYER_Y)):
 		for x in range(int(Globals.PLAYER_X)):
 
 			# Adding the colour rects to the scene tree
-			var new_cell      = ColorRect.new()
+			var new_cell      = ColorCell.new()
 			self.add_child(new_cell)
 
 			# Setting a unique name
 			new_cell.set_name("Cell_%dx_%dy" % [x, y])
 
-			# Setting the default cell colour
-			new_cell.set_color(Globals.DEFAULT_COL)
-
 			# Setting the scale and position
-			new_cell.size     = scale_set
 			new_cell.position = Vector2(x, y)
 
 			# Adding to list
